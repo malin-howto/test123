eb45c7d9c26fd2a5cce03a558469fba3ddb8badf
malin <61813569+malin-howto@users.noreply.github.com>
2023-05-01 19:18:17 +0700

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def load_data(sheets_url):
     return pd.read_csv(csv_url)
 
 df = load_data(st.secrets["public_gsheets_url"])
-
+print(df)
 # Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
